9b2e252e5ea1048e518decaff831e345b2cbf0d0
Emma Almström <emma.almstrom@skola.botkyrka.se>
2020-12-06 12:00:37 +0100

    upppgift 2

--- 49/1/script.js
+++ 49/1/script.js
@@ -19,4 +19,3 @@ for (let i = 0; i < 5; i++) {
 }
 
 console.log(str2)
-
---
+++ 49/2/script.js
@@ -0,0 +1,8 @@
+const str = '* * * * *\n'
+console.log(str.repeat(5))
+
+
+let str2 = '* '
+for (let i = 10; i > 0; i--) {
+    console.log(str2.repeat(i))
+}
